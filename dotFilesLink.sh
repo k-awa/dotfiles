eebddaccd99ee985e8632817aae0e45d73bfc7b5
k-awa <awahara.w@gmail.com>
2016-07-01 11:17:13 +0900

    Add making backup to dotFilesLink.sh

--- dotFilesLink.sh
+++ dotFilesLink.sh
@@ -1,4 +1,15 @@
 #!bin/sh
+if [ ! -d ~/dotfiles_bak ]; then
+  mkdir ~/dotfiles_bak
+  cp ~/.bashrc ~/dotfiles_bak 2>/dev/null
+  cp ~/.bash_logout ~/dotfiles_bak 2>/dev/null
+  cp ~/.gitconfig ~/dotfiles_bak 2>/dev/null
+  cp ~/.tmux.conf ~/dotfiles_bak 2>/dev/null
+  cp -r ~/.vim ~/dotfiles_bak 2>/dev/null
+  cp ~/.vimrc ~/dotfiles_bak 2>/dev/null
+fi
+
+
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_logout ~/.bash_logout
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
